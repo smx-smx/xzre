fd1886c6330c5cf3c55085d6ae65d89b74cf3314
smx-smx <smxdev4@gmail.com>
2024-04-03 23:00:32 +0000

    Deploying to gh-pages from @ smx-smx/xzre@cfbe60d6908fc56dcdcfa03dd3a321ab26d76ef3 🚀

--- search/all_4.js
+++ search/all_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['index_10',['index',['../unionsecret__data__shift__cursor.html#a9b139f7265c8349205b28aa6f8d894e0',1,'secret_data_shift_cursor']]]
+  ['index_11',['index',['../unionsecret__data__shift__cursor.html#a9b139f7265c8349205b28aa6f8d894e0',1,'secret_data_shift_cursor']]]
 ];
--- search/all_5.js
+++ search/all_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['secret_5fdata_5fappend_5fif_5fflags_11',['secret_data_append_if_flags',['../xzre_8h.html#ad21c1f0b4b9127ea1234d46dbadc3e8b',1,'xzre.h']]],
-  ['secret_5fdata_5fappend_5fsingleton_12',['secret_data_append_singleton',['../xzre_8h.html#a36a945c9790681a50af0a1a0cd72a269',1,'xzre.h']]],
-  ['secret_5fdata_5fshift_5fcursor_13',['secret_data_shift_cursor',['../unionsecret__data__shift__cursor.html',1,'']]]
+  ['secret_5fdata_5fappend_5fif_5fflags_12',['secret_data_append_if_flags',['../xzre_8h.html#ad21c1f0b4b9127ea1234d46dbadc3e8b',1,'xzre.h']]],
+  ['secret_5fdata_5fappend_5fsingleton_13',['secret_data_append_singleton',['../xzre_8h.html#a36a945c9790681a50af0a1a0cd72a269',1,'xzre.h']]],
+  ['secret_5fdata_5fshift_5fcursor_14',['secret_data_shift_cursor',['../unionsecret__data__shift__cursor.html',1,'']]]
 ];
