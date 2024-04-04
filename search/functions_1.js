9e4f5c77f667b00a337e6d5edfe8d8b9a387b9c3
smx-smx <smxdev4@gmail.com>
2024-04-04 19:30:29 +0000

    Deploying to gh-pages from @ smx-smx/xzre@42f3f0b1b980425bb640b36d44f75081b9ad8b75 🚀

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['elf_5fcontains_5fsegment_24',['elf_contains_segment',['../xzre_8h.html#a484c7f2d91a4774ef5c9de803c9fcfa5',1,'xzre.h']]],
+  ['elf_5fcontains_5fvaddr_24',['elf_contains_vaddr',['../xzre_8h.html#a2a09d15b1c506e0f29ee9433771ad8db',1,'xzre.h']]],
   ['elf_5fget_5fcode_5fsegment_25',['elf_get_code_segment',['../xzre_8h.html#af3f0d23e5fece210bdf4945c65e3a10a',1,'xzre.h']]],
   ['elf_5fparse_26',['elf_parse',['../xzre_8h.html#a049328971f4e99ce954d5e0346fee6d7',1,'xzre.h']]],
   ['elf_5fsymbol_5fget_27',['elf_symbol_get',['../xzre_8h.html#ac0a43ada7f303cb88b9c4b48a17b5154',1,'xzre.h']]],
